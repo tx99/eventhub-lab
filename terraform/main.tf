cfb80e3dc68ea6bc694eac134cf54d34fc3e9f45
Andrew Griffiths <andrew039@gmail.com>
2024-06-20 10:49:54 +0300

    flux test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,18 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.107.0" 
+    }
+  }
+}
+
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
 }
 
 data "azurerm_client_config" "current" {}
@@ -15,12 +28,12 @@ resource "azurerm_key_vault" "keyvault" {
   name                = "${var.resource_group_name}-kv"
   location            = azurerm_resource_group.eventhub_resource_group.location
   resource_group_name = azurerm_resource_group.eventhub_resource_group.name
-  tenant_id           = var.tenant_id
+  tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
 
   access_policy {
-    tenant_id = var.tenant_id
-    object_id = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
 
     key_permissions = [
       "Get",
@@ -74,6 +87,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
     type = "SystemAssigned"
   }
 
+
   network_profile {
     network_plugin = "azure"
     network_policy = "azure"
@@ -95,30 +109,32 @@ resource "random_password" "postgresql_admin_password" {
   override_special = "_%+="
 }
 
-# PostgreSQL Flexible Server
-resource "azurerm_postgresql_flexible_server" "booksdb" {
-  name                   = "${var.resource_group_name}-flexibleserver"
-  resource_group_name    = azurerm_resource_group.eventhub_resource_group.name
-  location               = azurerm_resource_group.eventhub_resource_group.location
-  version                = "13"
-  administrator_login    = "psqladminun"
-  administrator_password = random_password.postgresql_admin_password.result
-  storage_mb             = 32768
-  sku_name               = "B_Standard_B1ms"
+# Azure Database for PostgreSQL - Single Server
+resource "azurerm_postgresql_server" "booksdb" {
+  name                     = "${var.resource_group_name}-postgresql"
+  resource_group_name      = azurerm_resource_group.eventhub_resource_group.name
+  location                 = azurerm_resource_group.eventhub_resource_group.location
+  version                  = "11"
+  administrator_login      = "psqladminun"
+  administrator_login_password = random_password.postgresql_admin_password.result
+  sku_name                 = "B_Gen5_1"
+  storage_mb               = 5120
+  ssl_enforcement_enabled  = true  # Adjust according to your security requirements
 
-  maintenance_window {
-    day_of_week  = 5
-    start_hour   = 3
-  }
+  backup_retention_days        = 7
+  geo_redundant_backup_enabled = false
+  auto_grow_enabled            = false
 }
 
 # PostgreSQL Database
-resource "azurerm_postgresql_flexible_server_database" "booksdb" {
-  name      = "booksdb"
-  server_id = azurerm_postgresql_flexible_server.booksdb.id
-  charset   = "UTF8"
-  collation = "en_US.utf8"
+resource "azurerm_postgresql_database" "booksdb" {
+  name                = "booksdb"
+  resource_group_name = azurerm_resource_group.eventhub_resource_group.name
+  server_name         = azurerm_postgresql_server.booksdb.name
+  charset             = "UTF8"
+  collation           = "en_US.utf8"
 }
+
 
 # Store the PostgreSQL admin username and password in Key Vault
 resource "azurerm_key_vault_secret" "postgresql_admin_username" {
@@ -133,10 +149,11 @@ resource "azurerm_key_vault_secret" "postgresql_admin_password" {
   key_vault_id = azurerm_key_vault.keyvault.id
 }
 
-# Construct the PostgreSQL connection string and store it in Key Vault
 locals {
-  postgresql_connection_string = "Host=${azurerm_postgresql_flexible_server.booksdb.fqdn};Database=booksdb;Username=${azurerm_postgresql_flexible_server.booksdb.administrator_login}@${azurerm_postgresql_flexible_server.booksdb.name};Password=${random_password.postgresql_admin_password.result};Port=5432;"
+  postgresql_connection_string = "Host=${azurerm_postgresql_server.booksdb.fqdn};Database=booksdb;Username=${azurerm_postgresql_server.booksdb.administrator_login}@${azurerm_postgresql_server.booksdb.name};Password=${random_password.postgresql_admin_password.result};Port=5432;"
 }
+
+
 
 resource "azurerm_key_vault_secret" "postgresql_connection_string" {
   name         = "postgresql-connection-string"
@@ -163,18 +180,17 @@ resource "azurerm_kubernetes_flux_configuration" "k8s_flux" {
 
   kustomizations {
     name                      = "ingress"
-    path                      = "./templates/"
+    path                      = "/templates/"
     sync_interval_in_seconds  = 120
     retry_interval_in_seconds = 120
   }
 
   kustomizations {
     name                      = "frontend"
-    path                      = "./src/frontend/k8"
+    path                      = "/src/frontend/k8"
     sync_interval_in_seconds  = 120
     retry_interval_in_seconds = 120
   }
-
 
   scope = "cluster"
 
